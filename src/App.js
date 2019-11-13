ece653faac2cbafd8b7ace5628888af8370ef18d
KY <himach1234@gmail.com>
2019-11-14 00:27:29 +0300

    add message

--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 
 const App = (props) => {
 
-  return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Route path='/dialogs'
-            render={() => <Dialogs
-              state={props.state.dialogsPage} />} />
-          <Route path='/profile'
-            render={() => <Profile
-              profilePage={props.state.profilePage}
-              dispatch={props.dispatch}/>} />
-          {/* <Route path='/news' component={News} />
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs'
+                           render={() => <Dialogs
+                               store={props.store}/>}/>
+                    <Route path='/profile'
+                           render={() => <Profile
+                               profilePage={props.state.profilePage}
+                               dispatch={props.dispatch}/>}/>
+                    {/* <Route path='/news' component={News} />
       <Route path='/music' component={Music} />
       <Route path='/settings' component={Settings} /> */}
-          {/* <Profile />
+                    {/* <Profile />
       <Dialogs /> */}
-        </div>
-      </div>
-    </BrowserRouter>);
-}
+                </div>
+            </div>
+        </BrowserRouter>);
+};
 
 export default App;
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,57 +1,78 @@
-let store ={
-    _state : {
-        profilePage : {
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
+
+let store = {
+    _state: {
+        profilePage: {
             posts: [
-                { id: 1, message: 'Hi, bro!', likeCount: 12 },
-                { id: 2, message: 'How are you?', likeCount: 21 },
+                {id: 1, message: 'Hi, bro!', likeCount: 12},
+                {id: 2, message: 'How are you?', likeCount: 21},
             ],
-            newPostText : ['vrym']
+            newPostText: ['vrym']
         },
-    
+
         dialogsPage: {
             dialogs: [
-                { id: 1, name: 'Zenya' },
-                { id: 2, name: 'Sasha' },
-                { id: 3, name: 'Max' },
-                { id: 4, name: 'Dima' },
-                { id: 5, name: 'Nastya' },
+                {id: 1, name: 'Zenya'},
+                {id: 2, name: 'Sasha'},
+                {id: 3, name: 'Max'},
+                {id: 4, name: 'Dima'},
+                {id: 5, name: 'Nastya'},
             ],
-    
+
             messages: [
-                { id: 1, message: 'Hi' },
-                { id: 2, message: 'Hello' },
-                { id: 3, message: 'How are you?' },
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Hello'},
+                {id: 3, message: 'How are you?'},
             ],
+            newMessageBody: ""
         },
-    
-    
+
+
     },
-    getState(){
+    getState() {
         return this._state;
     },
     _callSubscriber() {
         console.log('state')
     },
-    subscribe(observer)  {
+    subscribe(observer) {
         this._callSubscriber = observer
     },
     dispatch(action) {
-      if (action.type === 'ADD-POST') {
-          let newPost = {
-              id:5,
-              message: this._state.profilePage.newPostText,
-              likeCount:0,
-          };
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likeCount: 0,
+            };
 
-          this._state.profilePage.posts.push(newPost);
-          this._state.profilePage.newPostText= '';
-          this._callSubscriber(this._state);
-      } else if (action.type === 'UPDATE-NEW-POST-TEXT' ) {
-          this._state.profilePage.newPostText = action.newText;
-          this._callSubscriber(this._state);
-      }
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE_NEW_MESSAGE_BODY') {
+            this._state.dialogsPage.newMessageBody = action.body;
+            this._callSubscriber(this._state);
+        } else if (action.type === 'SEND_MESSAGE') {
+            let body = this._state.dialogsPage.newMessageBody ;
+            this._state.dialogsPage.newMessageBody = '';
+            this._state.dialogsPage.messages.push({id: 4, message: body});
+            this._callSubscriber(this._state);
+        }
     }
 };
+export const addPostActionCreator = () =>({type: ADD_POST});
+export const updateNewPostTextActionCreator = (text) =>
+    ({type:UPDATE_NEW_POST_TEXT, newText: text});
+
+export const sendMessageCreator = () =>({type: SEND_MESSAGE});
+export const updateNewMessageBodyCreator = (body) =>
+    ({type:UPDATE_NEW_MESSAGE_BODY, body: body});
 
 export default store;
 window.store = store;
