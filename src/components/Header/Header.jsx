5c19dbcef11093cde9f41ad3088cb91102f1c18a
KY <himach1234@gmail.com>
2020-01-11 22:33:23 +0300

    cookie, login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 
-
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
         <img src='https://git-scm.com/images/logos/downloads/Git-Icon-Black.png'></img>
+        <div className={s.loginBlick}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 
