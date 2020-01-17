ac8636f9aefff17a37877258dc16f01bf666bf31
KY <himach1234@gmail.com>
2020-01-17 21:50:14 +0300

    redux-thunk 2

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,34 @@
 import * as axios from "axios";
 
 const instance = axios.create({
-    baseURL : 'https://social-network.samuraijs.com/api/1.0/',
-    withCredentials:true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
     headers: {
-        "API-KEY":"94e81fc4-4e03-4aff-a601-8c8664f96c04"
+        "API-KEY": "94e81fc4-4e03-4aff-a601-8c8664f96c04"
     }
 });
 
 
-export const usersAPI= {getUsers(currentPage, pageSize) {
-        return  instance.get(`users?page=${currentPage}&count=${pageSize}`)
+export const usersAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => {
                 return response.data;
             })
     },
     follow(userId) {
-       return  instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+        return instance.post(`follow/${userId}`)
     },
     unfollow(userId) {
-        return  instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+        return instance.delete(`follow/${userId}`)
     },
+    getProfile(userId) {
+        return instance.get(`profile/` + userId)
+    }
+}
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
+    }
 }
